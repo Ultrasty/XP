70db2770318655e2c210a316366a2e9ded9a9784
optimistickz <290077052@qq.com>
2021-01-12 19:25:32 +0800

    删除一些注释

--- backend/exam/src/main/java/com/tongji/exam/entity/Question.java
+++ backend/exam/src/main/java/com/tongji/exam/entity/Question.java
@@ -24,17 +24,12 @@ public class Question {
     private String questionDescription;
     private String questionOptionIds;
     private String questionAnswerOptionIds;
-    /**
-     * 创建时间, 设计表时设置了自动插入当前时间，无需在Java代码中设置了
-     */
+
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
-    /**
-     * 更新时间，设计表时设置了自动插入当前时间，无需在Java代码中设置了。
-     * 同时@DynamicUpdate注解可以时间当数据库数据变化时自动更新，无需人工维护
-     */
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 }
