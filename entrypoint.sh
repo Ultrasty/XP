9e901e149f321b476064ddabaeb36b66d898a8cd
sty <1455670697@qq.com>
2021-01-13 14:46:29 +0800

    delete

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-nginx
-nohup java -jar exam-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
-
--- make_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd backend/exam/ && mvn clean package
-cd ../../frontend/exam/ && npm install && npm run build
-cd ../../ && docker build -t waterknife/exam .
